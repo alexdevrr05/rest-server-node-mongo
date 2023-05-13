faccc8b535d7b02edd6fd75c701fe5c454b13785
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2023-05-13 12:02:54 -0600

    unique filename multer config

--- helpers/multerStorage.js
+++ helpers/multerStorage.js
@@ -1,11 +1,15 @@
+const path = require('path');
 const multer = require('multer');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, 'upload/');
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    const uniqueFilename = Date.now(); // Genera un identificador único basado en la fecha actual en milisegundos
+    const fileExtension = path.extname(file.originalname); // Obtiene la extensión del archivo original
+    const fileName = uniqueFilename + fileExtension; // Concatena el identificador con la extensión del archivo original
+    cb(null, fileName); // Asigna el
   },
   fileFilter: function (req, file, cb) {
     // Verifica la extensión del archivo
